d8659898036bdb651fae515a785e0ce3d87bc605
unknown <iasminsena6013@digitalcollege.internal>
2025-02-06 21:47:20 -0300

    Aula15

--- js/pagamento.js
+++ js/pagamento.js
@@ -34,10 +34,28 @@ const SPAN_NUMERO = document.getElementById('resultado_numero');
 const SPAN_TITULAR = document.getElementById('resultado_titular');
 const SPAN_MES = document.getElementById('resultado_mes');
 const SPAN_ANO = document.getElementById('resultado_ano');
+const IMG_VISA = document.getElementById('bandeira_visa');
+const IMG_MASTER = document.getElementById('bandeira_mastercard');
+const SPAN_CVV = document.getElementById('resultado_cvv');
 
+
+function removerUltimoDigito(numero){
+    return numero.substr(0, numero.length - 1);
+}
 
 function preencherNumero() {
-    SPAN_NUMERO.innerHTML = INPUT_NUMERO.value; 
+    let digitos = INPUT_NUMERO.value;
+    let ultimoDigito = digitos.substr(-1);
+
+    if (isNaN(ultimoDigito)){
+       digitos = removerUltimoDigito(digitos);
+    }
+
+    INPUT_NUMERO.value = digitos;
+    SPAN_NUMERO.innerHTML = digitos;
+
+    alterarBandeira();
+
 }
 
 function preencherTitular() {
@@ -56,4 +74,47 @@ function preencherMes() {
 
 function preencherAno() {
     SPAN_ANO.innerHTML = SELECT_ANO.value; 
+}
+
+function alterarBandeira(){
+    if (INPUT_NUMERO.value.length > 1){
+        return;
+    }
+
+    IMG_VISA.style.display = 'none';
+    IMG_MASTER.style.display = 'none';
+
+    if (INPUT_NUMERO.value === '4') {
+        IMG_VISA.style.display = 'block';
+    }
+
+    if (INPUT_NUMERO.value === '5') {
+        IMG_MASTER.style.display = 'block';
+    }
+}
+
+function preencherCVV() {
+    let digitos = INPUT_CVV.value;
+    let ultimoDigito = digitos.substr(-1);
+
+    if (isNaN(ultimoDigito)){
+       digitos = removerUltimoDigito(digitos);
+    }
+
+    INPUT_CVV.value = digitos;
+    SPAN_CVV.innerHTML = digitos; 
+}
+
+function mostrarFrente(){
+    document.getElementById('cartao_frente').classList.add('animate_slideInRight')
+
+    document.getElementById('cartao_frente').style.display = 'block';
+    document.getElementById('cartao_verso').style.display = 'none';
+}
+
+function mostrarVerso(){
+    document.getElementById('cartao_verso').classList.add('animate__slideInRight')
+
+    document.getElementById('cartao_verso').style.display = 'block';
+    document.getElementById('cartao_frente').style.display = 'none';
 }
