a7a35cdd736781cd83fa74d4f472718bb5d79b8b
unknown <iasminsena6013@digitalcollege.internal>
2025-03-20 20:51:54 -0300

    adicionar novo produto

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # clubedovinho-front
 Aplicação em HTML, CSS, bootstrap para o frontend de um ecommerce de vinhos
+
+## Como rodar a aplicação
+1 - Abrir com o navegador o index.html do clubedovinho-front
+2 - Subir o servidor que simula o vando de dados 
+''' shell 
+npx json-server db.json
+'''
--- js/produto.js
+++ js/produto.js
@@ -1,40 +1,33 @@
 const TABELA_PRODUCTS = document.getElementById('table-product');
 
-let produto = [
-    {
-        id: 1,
-        nome: 'Quinta do Morgado',
-        categoria: 'Vinho Tinto Suave',
-        imagem: 'https://cdn.dooca.store/197/products/vinho-quinta-do-morgado-tinto-suave-750-ml.jpg?v=1665008400&webp=0',
-        quantidade: 10,
-        valor: 10.90
-    },
-    {
-        id: 2,
-        nome: 'Vale Dom Bento',
-        categoria: 'Vinho Tinto Suave',
-        imagem: 'https://cdn.awsli.com.br/1958/1958204/produto/203245775/screenshot_20230223_190651_instagram-pamtvr.jpg',
-        quantidade: 24,
-        valor: 8.90
-    }
-];
-onclick
-produto.forEach(addLinha);
+listar();
 
-function addLinha(cada_produto){
+function listar(){
+    fetch('http://localhost:3000/products')
+        .then(res => res.json())
+        .then(dados => {
+            TABELA_PRODUCTS.innerHTML = '';
+            dados.forEach(addLinha);
+        });
+};
+
+onclick
+
+function addLinha(cada_produto) {
+
     TABELA_PRODUCTS.innerHTML += `
     <tr>
         <td>${cada_produto.id}</td>
-        <td>${cada_produto.nome}</td>
-        <td>${cada_produto.categoria}</td>
-        <td> <img onclick= "abrirModal('${cada_produto.nome}', ' ${cada_produto.imagem}')" data-bs-toggle="modal" data-bs-target="#exampleModal" src="${cada_produto.imagem}" width="100px"></td>
-        <td>${cada_produto.quantidade}</td>
-        <td>${cada_produto.valor}</td>
+        <td>${cada_produto.name}</td>
+        <td>${cada_produto.category}</td>
+        <td> <img onclick= "abrirModal('${cada_produto.name}', ' ${cada_produto.image}')" data-bs-toggle="modal" data-bs-target="#exampleModal" src="${cada_produto.image}" width="100px"></td>
+        <td>${cada_produto.quantity}</td>
+        <td>${cada_produto.price}</td>
         <td>
             <a href="#" class="btn btn-outline-warning btn-sm">
                 Editar
             </a>
-            <a href="#" class="btn btn-outline-danger btn-sm">
+            <a href="#" onclick="excluir('${cada_produto.id}')" class="btn btn-outline-danger btn-sm">
                 Excluir
             </a>
         </td>
@@ -42,9 +35,47 @@ function addLinha(cada_produto){
     `;
 }
 
-function abrirModal(nome, imagem){
-    document.getElementById('modal_produto_nome').innerHTML= nome;
-    document.getElementById('modal_produto_imagem').innerHTML= `
+function excluir(id) {
+    if (false === confirm('Quer excluir esse produto?')){
+        return;
+    }
+
+    fetch(`http://localhost:3000/products/${id}` ,{
+        method:"DELETE"
+    });
+
+    alert('Excluido com sucesso');
+    listar();
+}
+
+
+function abrirModal(nome, imagem) {
+    document.getElementById('modal_produto_nome').innerHTML = nome;
+    document.getElementById('modal_produto_imagem').innerHTML = `
         <img src="${imagem}" width="100%">
     `;
+}
+
+function addProduct(){
+    event.preventDefault();
+
+    let dados= {
+        name: document.getElementById('name').value,
+        price: document.getElementById('price').value,
+        category: document.getElementById('category').value,
+        quantity: document.getElementById('quantity').value,
+        image: document.getElementById('image').value
+    };
+
+    fetch('http://localhost:3000/products',{
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(dados)
+    })
+
+    document.getElementById('form').requestFullscreen();
+    alert('Produto cadastrado com sucesso');
+    listar();
 }
